d00284b962aedb658449f8992dc2cbc4d37a013e
NiharSadhu <113557223+NiharSadhu@users.noreply.github.com>
2023-03-08 14:44:24 -0500

    node server started

--- nodeServer/index.js
+++ nodeServer/index.js
@@ -3,3 +3,15 @@
 const io = require('socket.io')(8000)
 
 const users = {};
+
+io.on('connection', socket =>{
+    socket.on('user-joined', name =>{
+        users[socket.id] = name;
+        socket.broadcast.emit('user-joined', name)
+    });
+
+    socket.on('send', message =>{
+        socket.broadcast.emit('recieve', {message: message, name: user[socket.id]})
+    });
+})
+
